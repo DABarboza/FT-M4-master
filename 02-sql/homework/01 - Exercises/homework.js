1639f53590c5d25de2cb675e64f26ab7a1316560
DAbarboza <bardanii7@gmail.com>
2024-04-29 21:22:06 -0300

    Act 29/04/24

--- 02-sql/homework/01 - Exercises/homework.js
+++ 02-sql/homework/01 - Exercises/homework.js
@@ -1,22 +1,24 @@
 /* ¡Escribe tus comandos en este archivo! */
 
-const ejercicio02 = '';
+const ejercicio02 = "SELECT * FROM movies WHERE duration < 90;";
 
-const ejercicio03 = '';
+const ejercicio03 =
+  "SELECT * FROM movies WHERE year >= 1930 AND year <=1940;" ||
+  "SELECT * FROM movies WHERE year BETWEEN 1930 AND 1940;";
 
-const ejercicio04 = '';
+const ejercicio04 = "SELECT * FROM movies WHERE title ILIKE '%til%';";
 
-const ejercicio05 = '';
+const ejercicio05 = "";
 
-const ejercicio06 = '';
+const ejercicio06 = "";
 
-const ejercicio07 = '';
+const ejercicio07 = "";
 
 module.exports = {
-   ejercicio02,
-   ejercicio03,
-   ejercicio04,
-   ejercicio05,
-   ejercicio06,
-   ejercicio07,
+  ejercicio02,
+  ejercicio03,
+  ejercicio04,
+  ejercicio05,
+  ejercicio06,
+  ejercicio07,
 };
